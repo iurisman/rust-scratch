32d7af7e0664edfead7f4be45c486330c22d9ee4
Igor Urisman <igor@Igors-MacBook-Pro.local>
2025-03-06 13:33:22 -0800

    WIP

---
+++ BinarySearchTree/target/debug/deps/binary_search_tree-9c5e67d63cd570b3.d
@@ -0,0 +1,7 @@
+/Users/igor/work/src/rust-scratch/BinarySearchTree/target/debug/deps/libbinary_search_tree-9c5e67d63cd570b3.rmeta: src/lib.rs
+
+/Users/igor/work/src/rust-scratch/BinarySearchTree/target/debug/deps/libbinary_search_tree-9c5e67d63cd570b3.rlib: src/lib.rs
+
+/Users/igor/work/src/rust-scratch/BinarySearchTree/target/debug/deps/binary_search_tree-9c5e67d63cd570b3.d: src/lib.rs
+
+src/lib.rs:
--- Scratch/src/bin_search_tree.rs
+++ Scratch/src/bin_search_tree.rs
@@ -13,20 +13,27 @@ impl <T:Ord + Copy + Debug> Node<T> {
 
     fn new(t: T) -> Node<T> {Node { data:t, left: None, right: None } }
 
-    fn insert(&mut self, t: T) {
+    fn insert(&mut self, t: T) -> bool {
         match self.data.cmp(&t) {
             Less => match  &mut self.right {
-                None => self.right = Some(Box::new(Node::new(t))),
+                None => {
+                    self.right = Some(Box::new(Node::new(t)));
+                    true
+                },
                 Some(ref mut right) => {
-                    let foo = right.as_mut();
-                    foo.insert(t);
+                    right.as_mut().insert(t)
                 }
             }
-            Greater => match &self.left {
-                None => self.left = Some(Box::new(Node::new(t))),
-                Some(_) => panic!("Cannot insert a left twice"),
+            Greater => match &mut self.left {
+                None => {
+                    self.left = Some(Box::new(Node::new(t)));
+                    true
+                },
+                Some(ref mut left) => {
+                    left.as_mut().insert(t)
+                }
             }
-            Equal => println!("Ignored duplicate key: {:?}", t),
+            Equal => false
         }
     }
 }
@@ -36,12 +43,17 @@ struct BinSearchTree<T: Ord + Copy>(Option<Node<T>>);
 impl <T: Ord + Copy + Debug>BinSearchTree<T> {
     pub fn new() -> BinSearchTree<T>{BinSearchTree(None)}
 
-
+    /// New instance of tree node.
     pub fn is_empty(&self) -> bool { self.0.is_none() }
 
-    pub fn insert(&mut self, t: T) -> () {
+    /// Insert a new key in the tree.
+    /// Returns `true` if the insert was successful, or `false` if the key already exists.
+    pub fn insert(&mut self, t: T) -> bool {
         match &mut self.0 {
-            None => self.0 = Some(Node::new(t)),
+            None => {
+                self.0 = Some(Node::new(t));
+                true
+            },
             Some(r) => r.insert(t)
         }
     }
@@ -55,9 +67,11 @@ mod tests {
     fn test_bin_search_tree() {
         let mut tree: BinSearchTree<i32> = BinSearchTree::new();
         assert!(tree.is_empty());
-        tree.insert(10);
+        assert!(tree.insert(10));
+        assert!(!tree.insert(10));
         assert!(!tree.is_empty());
-        tree.insert(11);
-        tree.insert(11);
+        assert!(tree.insert(11));
+        assert!(!tree.insert(11));
+        assert!(!tree.insert(10));cd
     }
 }
