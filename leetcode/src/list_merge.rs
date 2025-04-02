fcc5102eb40fd5d4a11b9a9cbe3dbc59b739b288
Igor Urisman <igor@Igors-MacBook-Pro.local>
2025-04-01 18:59:31 -0700

    WIP

---
+++ leetcode/src/list_merge.rs
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+fn merge<T: Ord + Clone>(vec1: Vec<T>, vec2: Vec<T>) -> Vec<T> {
+
+    let mut merged: Vec<T> = vec![];
+
+    let mut iter1 = vec1.into_iter();
+    let mut iter2 = vec2.into_iter();
+    let mut next1_opt= iter1.next();
+    let mut next2_opt= iter2.next();
+
+    while next1_opt.is_some() || next2_opt.is_some() {
+        match (&next1_opt, &next2_opt) {
+            (Some(t1), Some(t2)) => {
+                if *t1 > *t2 {
+                    merged.push((*t1).to_owned());
+                    next1_opt = iter1.next();
+                } else {
+                    merged.push((*t2).to_owned());
+                    next2_opt = iter2.next();
+                }
+            }
+            (Some(t1), None) => {
+                merged.push((*t1).to_owned());
+                next1_opt = iter1.next();
+            }
+            (None, Some(t2)) => {
+                merged.push((*t2).to_owned());
+                next2_opt = iter2.next();
+            }
+            (None, None) => {
+                next1_opt = None;
+                next2_opt = None;
+            }
+        }
+    }
+    merged
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::list_merge::merge;
+    #[test]
+    fn test() {
+        let l1 = vec!['a', 'c', 'e', 'g'];
+        let l2 = vec!['b', 'd', 'f', 'h', 'i'];
+        println!("{:?}", merge(l1, l2));
+    }
+}
--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,11 +1,39 @@
 
 mod add_two_numbers;
 mod longest_substring;
+mod list_merge;
 fn main() {
-    println!("{}", mk_slice("foo bar baz"));
+    let l1 = vec!['a', 'c', 'e', 'g'];
+    let l2 = vec!['b', 'd', 'f', 'h', 'i'];
+    println!("{:?}", merge(l1, l2));
 }
 
-fn mk_slice(string: &str) -> &str {
-    &string[2..3]
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
 
+fn merge<T: Ord>(vec1: Vec<T>, vec2: Vec<T>) -> Vec<T> {
+
+    let mut merged: Vec<T> = vec![];
+
+    let mut iter1 = vec1.into_iter();
+    let mut next1_opt= iter1.next();
+
+    while next1_opt.is_some() {
+        match next1_opt {
+            Some(t) => {
+                if t > t {
+                    merged.push(t);
+                    next1_opt = iter1.next();
+                }
+                else {
+                    next1_opt = None;
+                }
+            }
+            None => {
+                next1_opt = iter1.next();
+            }
+        }
+
+
+    }
+    merged
 }
