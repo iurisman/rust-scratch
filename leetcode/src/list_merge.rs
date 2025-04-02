99caef1dd8c6731cec7ee26541ddde355fde301d
Igor Urisman <igor@Igors-MacBook-Pro.local>
2025-04-02 14:25:04 -0700

    WIP

--- leetcode/src/list_merge.rs
+++ leetcode/src/list_merge.rs
@@ -1,9 +1,15 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/
 
+fn try_push<T: Ord + Clone>(vec: &mut Vec<T>, t: &T) {
+    match vec.last() {
+        None => vec.push(t.clone()),
+        Some(last) => if *t > *last {vec.push(t.clone())},
+    }
+}
+
 fn merge<T: Ord + Clone>(vec1: Vec<T>, vec2: Vec<T>) -> Vec<T> {
 
     let mut merged: Vec<T> = vec![];
-
     let mut iter1 = vec1.into_iter();
     let mut iter2 = vec2.into_iter();
     let mut next1_opt= iter1.next();
@@ -12,20 +18,20 @@ fn merge<T: Ord + Clone>(vec1: Vec<T>, vec2: Vec<T>) -> Vec<T> {
     while next1_opt.is_some() || next2_opt.is_some() {
         match (&next1_opt, &next2_opt) {
             (Some(t1), Some(t2)) => {
-                if *t1 > *t2 {
-                    merged.push((*t1).clone());
+                if *t1 < *t2 {
+                    try_push(&mut merged, t1);
                     next1_opt = iter1.next();
                 } else {
-                    merged.push((*t2).clone());
+                    try_push(&mut merged, t2);
                     next2_opt = iter2.next();
                 }
             }
             (Some(t1), None) => {
-                merged.push((*t1).clone());
+                try_push(&mut merged, t1);
                 next1_opt = iter1.next();
             }
             (None, Some(t2)) => {
-                merged.push((*t2).clone());
+                try_push(&mut merged, t2);
                 next2_opt = iter2.next();
             }
             (None, None) => {}
@@ -39,7 +45,7 @@ mod tests {
     use crate::list_merge::merge;
     #[test]
     fn test() {
-        let l1 = vec!['a', 'c', 'e', 'g'];
+        let l1 = vec!['a', 'c', 'e', 'g', 'h', 'i'];
         let l2 = vec!['b', 'd', 'f', 'h', 'i'];
         println!("{:?}", merge(l1, l2));
     }
--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -3,27 +3,5 @@ mod add_two_numbers;
 mod longest_substring;
 mod list_merge;
 fn main() {
-}
-
-// https://leetcode.com/problems/median-of-two-sorted-arrays/
-
-fn f1<T: Ord>(vec1: Vec<T>) -> Vec<T> {
-
-    let mut merged: Vec<T> = vec![];
-
-    let mut iter1 = vec1.into_iter();
-    let mut next1_opt= iter1.next();
-
-    // while next1_opt.is_some() {
-    //     match next1_opt {
-    //         Some(t) => {
-    //             if true {
-    //                 merged.push(t);
-    //                 next1_opt = iter1.next();
-    //             }
-    //         }
-    //         None => {}
-    //     }
-    // }
-    merged
+    println!("{}", 'a' < 'b')
 }
